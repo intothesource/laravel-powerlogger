d97280208cbaa845b831923edbf468b485185a96
Ramon Smit <ramon@daltcore.com>
2016-05-26 13:06:22 +0200

    whoops

--- src/config/powerlogger.php
+++ src/config/powerlogger.php
@@ -1,9 +1,9 @@
 <?php
 	return [
 	   'customer'   => 'Customer Name',           // Customer name eg. Into The Source
-       'domain'     => 'test.dev',                // FQDN eg intothesource.com
-       'slack'      => 'AAA/BBB/123'              // Slack key
-       'accept_env' => [						  // Add the environment when the system needs to log
+       'domain'     => 'test.dev',              // FQDN eg intothesource.com
+       'slack'      => 'AAA/BBB/123',           // Slack key
+       'accept_env' => [						            // Add the environment when the system needs to log
        		'production',
        		'local'
        ],
@@ -14,7 +14,7 @@
         *
         * Filters on request url and referer url!
         */
-       'filters' => [							  // Filter these url's and/or referers (Using: builded PCRE)
+       'filters' => [							             // Filter these url's and/or referers (Using: builded PCRE)
        		// 'www.google.com',
        		// 'internal.site.org',
        		// 'old/images/ico.png',
